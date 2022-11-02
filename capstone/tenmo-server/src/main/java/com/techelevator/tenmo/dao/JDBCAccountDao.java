96e1f1201bc262b231cf17c2c0ea4dfc89b4b0b1
KApolinario1120 <kevinsquared@gmail.com>
2022-11-02 11:10:23 -0400

    Add transfer model

--- capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/JDBCAccountDao.java
+++ capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/JDBCAccountDao.java
@@ -1,4 +1,32 @@
 package com.techelevator.tenmo.dao;
 
-public class JDBCAccountDao {
+import com.techelevator.tenmo.model.Account;
+
+import java.math.BigDecimal;
+
+public class JDBCAccountDao implements AccountDao{
+    @Override
+    public BigDecimal getBalance(int userId) {
+        return null;
+    }
+
+    @Override
+    public BigDecimal addToBalance(BigDecimal amountToAdd, int id) {
+        return null;
+    }
+
+    @Override
+    public BigDecimal subtractFromBalance(BigDecimal amountToSubtract, int id) {
+        return null;
+    }
+
+    @Override
+    public Account findUserById(int userId) {
+        return null;
+    }
+
+    @Override
+    public Account findAccountById(int id) {
+        return null;
+    }
 }
