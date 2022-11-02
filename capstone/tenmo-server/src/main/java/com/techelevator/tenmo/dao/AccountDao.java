964e235e75264b7248411cb638f64cf07e1c80d7
KApolinario1120 <kevinsquared@gmail.com>
2022-11-02 10:53:11 -0400

    Add account model and DAO

--- capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
+++ capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
@@ -1,5 +1,11 @@
 package com.techelevator.tenmo.dao;
+import java.math.BigDecimal;
+import com.techelevator.tenmo.model.Account;
 
-
-public class AccountDao {
+public interface AccountDao {
+    BigDecimal getBalance(int userId);
+    BigDecimal addToBalance(BigDecimal amountToAdd, int id);
+    BigDecimal subtractFromBalance(BigDecimal amountToSubtract, int id);
+    Account findUserById(int userId);
+    public Account findAccountById(int id);
 }
---
+++ capstone/tenmo-server/src/main/java/com/techelevator/tenmo/model/Account.java
@@ -0,0 +1,34 @@
+package com.techelevator.tenmo.model;
+
+import java.math.BigDecimal;
+
+public class Account {
+
+    private int accountId;
+    private int userId;
+    private BigDecimal balance;
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+}
