af78bca3b8b4280332f89d33445592ebc7fff101
KApolinario1120 <kevinsquared@gmail.com>
2022-11-03 10:03:55 -0400

    Add requestTransfer, getPendingRequests, and updateTransferRequest methods in transferDAO

--- capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferDao.java
+++ capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferDao.java
@@ -10,5 +10,7 @@ public interface TransferDao {
     public List <Transfer> getAllTransfers(int userId);
     public Transfer getTransferById(int transactionId);
     public String sendTransfer(int userFromId, int userToId, BigDecimal amount);
-    
+    public String requestTransfer(int userFrom, int userTo, BigDecimal amount);
+    public List<Transfer> getPendingRequests(int userId);
+    public String updateTransferRequest(Transfer transfer, int statusId);
 }
