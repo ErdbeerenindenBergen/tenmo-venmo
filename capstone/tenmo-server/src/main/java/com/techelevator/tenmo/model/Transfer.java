4c871df298b864eea1348b5e6601dd8c42164e4e
KApolinario1120 <kevinsquared@gmail.com>
2022-11-04 12:10:09 -0400

    Remove transferId from transfer constructor. Fix corresponding methods and tests

--- capstone/tenmo-server/src/main/java/com/techelevator/tenmo/model/Transfer.java
+++ capstone/tenmo-server/src/main/java/com/techelevator/tenmo/model/Transfer.java
@@ -14,8 +14,7 @@ public class Transfer {
     private String userFrom;
     private String userTo;
 
-    public Transfer(int transferId, int transferTypeId, int transferStatusId, int accountFrom, int accountTo, BigDecimal amount) {
-        this.transferId = transferId;
+    public Transfer(int transferTypeId, int transferStatusId, int accountFrom, int accountTo, BigDecimal amount) {
         this.transferTypeId = transferTypeId;
         this.transferStatusId = transferStatusId;
         this.accountFrom = accountFrom;
